dad977fe22bbcc7fa7fb341e586d2349398c41de
JGQJJC1003 <907283098@qq.com>
2018-12-09 22:12:48 +0800

    last  1tijiao

--- DataAnalyze/Predict.py
+++ DataAnalyze/Predict.py
@@ -10,6 +10,7 @@ def yuce(x,y):
     citys = type_data.company_area.unique()
     #找出所有的业务类型
     work_type = type_data.work_type.unique()
+    #创建函数，返回查询值的下标
     def city_int(item):
         index = np.argwhere(citys == item)[0, 0]
         return index
@@ -18,16 +19,20 @@ def yuce(x,y):
         return index
     type_data['company_area'] = type_data['company_area'].map(city_int)
     type_data['work_type'] = type_data['work_type'].map(type_int)
-    city_index = city_int(city)
-    typex_index = type_int(work_types)
     #取出训练数据
     X_train = type_data[['work_type','company_area']]
     Y_train = type_data[['work_price']]
+    #使用KNN近邻算法
     KNN =KNeighborsClassifier(10)
     KNN.fit(X_train,Y_train.astype('int'))
-    X_test = [[city_index,typex_index]]
-    y_ = KNN.predict(X_test)[0]
-    print('本市的此业务价格大概为：',y_,'元')
+    try:
+        city_index = city_int(city)
+        typex_index = type_int(work_types)
+        X_test = [[city_index, typex_index]]
+        y_ = KNN.predict(X_test)[0]
+        print('本市的此业务价格大概为：', y_, '元')
+    except Exception as e:
+        print("无效查询")
 work_types = input("请输入业务类型:")
 city = input("请输入城市:")
 yuce(city,work_types)
